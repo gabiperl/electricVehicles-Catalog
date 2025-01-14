72b3274ec74b328c5093f1e2a01b9b21e56c11e7
gabi <gabriel.perl@gmail.com>
2025-01-14 14:09:08 +0100

    add,remove y localStorage

--- src/app/garage.service.ts
+++ src/app/garage.service.ts
@@ -6,14 +6,41 @@ import { Injectable } from '@angular/core';
 export class GarageService {
   // private saludo= "Hola desde el servicio";
   private myVehicles:any[] = [];
-  constructor() {}
+  constructor() {
+    if (localStorage.getItem('myVehicles'))
+    {
+      this.myVehicles = JSON.parse(localStorage.getItem('myVehicles') || '[]');
+    }
+  }
 
-  addToMyVehicles(vehicle:any){
+  addToMyVehicles(vehicle:any)
+  {
+    if (!this.isAlreadyInMyGarage(vehicle) )
+    {
+      this.myVehicles.push(vehicle);
+      localStorage.setItem('myVehicles', JSON.stringify(this.myVehicles));
+    }
+    else
     this.myVehicles.push(vehicle);
   }
 
-  getMyVehicles(){
+  isAlreadyInMyGarage(vehicle:any)
+  {
+    return this.myVehicles.find(v => v.id == vehicle.id);
+  }
+
+  getMyVehicles()
+  {
     return this.myVehicles;
+  }
+
+  removeVehicleFromGarage(vehicle:any)
+  {
+    this.myVehicles = this.myVehicles.filter(v => v.id != vehicle.id);
+    localStorage.setItem('myVehicles', JSON.stringify(this.myVehicles));
+
+    // let posicion = this.myVehicles.findIndex(v => v.id == vehicle.id);
+    // this.myVehicles.splice(posicion, 1);
   }
 
   // getSaludo() 
--- src/app/vehicle-card/vehicle-card.component.ts
+++ src/app/vehicle-card/vehicle-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { GarageService } from '../garage.service';
 
 @Component({
   selector: 'app-vehicle-card',
@@ -9,4 +10,23 @@ import { Component, Input } from '@angular/core';
 
 export class VehicleCardComponent {
 @Input() vehicle:any;
+
+constructor(private garageSrvc:GarageService) { }
+
+addVehicleToGarage(aVehicle:any){
+  this.garageSrvc.addToMyVehicles(aVehicle);
 }
+
+isAlreadyInMyGarage(vehicle:any)
+{
+  return this.garageSrvc.isAlreadyInMyGarage(vehicle);
+}
+
+removeVehicleFromGarage(vehicle:any)
+{
+  this.garageSrvc.removeVehicleFromGarage(vehicle);
+}
+
+}
+
+
